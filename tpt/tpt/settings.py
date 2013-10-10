690f5e6d95327f1d0403913aae1aed14f5a66ecb
Hans Jørgen Hoel <hansjorg@gmail.com>
2013-10-10 20:53:50 +0200

    Add static-collected files

--- tpt/tpt/settings.py
+++ tpt/tpt/settings.py
@@ -1,3 +1,4 @@
+import os
 import private_settings
 
 # Django settings for tpt project.
@@ -5,7 +6,7 @@ import private_settings
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-#PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -67,7 +68,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_PATH + '/../../static-collected/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
