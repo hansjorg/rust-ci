00f37a821c60a4be796cfa6d15f34ad0aa5010ad
Hans Jørgen Hoel <hansjorg@gmail.com>
2013-10-17 10:19:46 +0200

    Add new domain name

--- tpt/tpt/settings.py
+++ tpt/tpt/settings.py
@@ -33,7 +33,7 @@ SERVER_EMAIL = 'root@hiho.io'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.hiho.io', 'localhost']
+ALLOWED_HOSTS = ['.rust-ci.org', '.hiho.io', 'localhost']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
