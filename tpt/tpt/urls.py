e498830642cc7281fa1615ce0aeec64c9e33b0d6
Hans Jørgen Hoel <hansjorg@gmail.com>
2013-12-19 23:36:53 +0100

    Move to rust-ci.org

--- tpt/tpt/urls.py
+++ tpt/tpt/urls.py
@@ -10,17 +10,17 @@ urlpatterns = patterns('',
     # url(r'^$', 'tpt.views.home', name='home'),
     # url(r'^tpt/', include('tpt.foo.urls')),
     
-    url(r'^rust-ci/$', 'ppatrigger.views.index', name='index'),
-    url(r'^rust-ci/help/$', 'ppatrigger.views.help', name='help'),
-    url(r'^rust-ci/p/add/$', 'ppatrigger.views.add_project', name='add_project'),
-    url(r'^rust-ci/p/(?P<project_id>\d+)/trigger/$', 'ppatrigger.views.trigger_build', name='trigger_build'),
-    url(r'^rust-ci/p/(?P<project_id>\d+)/$', 'ppatrigger.views.show_project', name='show_project'),
+    url(r'^$', 'ppatrigger.views.index', name='index'),
+    url(r'^help/$', 'ppatrigger.views.help', name='help'),
+    url(r'^p/add/$', 'ppatrigger.views.add_project', name='add_project'),
+    url(r'^p/(?P<project_id>\d+)/trigger/$', 'ppatrigger.views.trigger_build', name='trigger_build'),
+    url(r'^p/(?P<project_id>\d+)/$', 'ppatrigger.views.show_project', name='show_project'),
     
-    url(r'^rust-ci/callback$', 'ppatrigger.views.github_callback'),
+    url(r'^callback$', 'ppatrigger.views.github_callback'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^rust-ci/admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
