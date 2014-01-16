9c49853e9afb704245926bb2603f3ee99660f623
Hans Jørgen Hoel <hansjorg@gmail.com>
2014-01-16 03:25:18 +0100

    Add imports

--- tpt/ppatrigger/admin.py
+++ tpt/ppatrigger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ppatrigger.models import Ppa, Package, Project
+from ppatrigger.models import Ppa, Package, Project, ProjectDocs, ProjectCategory
 
 admin.site.register(Ppa)
 admin.site.register(Package)
